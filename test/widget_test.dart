b3c086c3f7a7e14ec74a66262c4f5f0b908cf126
Francisco <bruceciscokid@gmail..com>
2022-10-14 17:55:59 -0500

    Principal

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Francisco Escobedo, Esta es una modificacion basica.', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
